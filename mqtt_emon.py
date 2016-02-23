515c61d246788afadcba90118e4292d3fafc057f
Ross Lazarus <ross.lazarus@gmail.com>
2016-02-24 10:41:58 +1100

    added an output file to collect tabular data with timestamp

--- mqtt_emon.py
+++ mqtt_emon.py
@@ -1,8 +1,12 @@
 # example from https://pypi.python.org/pypi/paho-mqtt
 # ross feb 23 2016
 # test direct access to emoncms
+# will append to outfile so delete to start again
 
 import paho.mqtt.client as mqtt
+import time
+
+OUTFNAME = 'mqtt_emon.xls'
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -10,13 +14,19 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    subs = client.subscribe("emonhub/rx/5/#")
-    print str(subs)
+    subs = client.subscribe("emonhub/rx/5/values")
+    print 'Subscription returned:', str(subs)
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-
+    #print(msg.topic+" "+str(msg.payload))
+    spay = msg.payload.replace(',','\t')
+    s = '%f\t%s' % (time.time(),spay)
+    outf.write(s)
+    outf.write('\n')
+    outf.flush()
+    
+outf = open(OUTFNAME,'a')
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
